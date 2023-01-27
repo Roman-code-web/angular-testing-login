bd1399734f0b47b72b72dcb47222722a31f6d649
Roman-code-web <selenerg.275@gmail.com>
2023-01-27 09:38:44 -0500

    formulario terminado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,63 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'testing-login';
+  //Declaro variables
+  title = 'Login';
+  formLogin!:FormGroup;
+  mensajeError={
+    usuario:'',
+    password:''
+  }
+  mensajeEnviado!:string;
+
+  //constructor
+  constructor(private frmb : FormBuilder){
+    this.formLogin=frmb.group({
+      usuario:['',
+      [
+        Validators.required,
+        Validators.minLength(5)
+      ]
+      ],
+      password:['',
+      [
+        Validators.required,
+        Validators.minLength(4)
+      ]
+      ]
+    })
+  }
+
+  //funcion
+  login(){
+    if(this.formLogin.valid){
+      this.mensajeEnviado="Datos enviados correctamente";
+      setTimeout(() => {
+        this.mensajeEnviado="";
+      }, 2000);
+    }
+    
+  }
+ //validacion formulario
+  validacion(name: string) {
+    if(this.formLogin.get(name)?.errors && (this.formLogin.get(name)?.touched || this.formLogin.get(name)?.dirty)){
+      if(this.formLogin.get(name)?.errors?.['required']){
+        if(name=='usuario'){
+          this.mensajeError.usuario="El campo es requerido"; 
+        }else{
+          this.mensajeError.password="El campo es requerido";
+        }
+        
+        
+      }else if(this.formLogin.get(name)?.errors?.['minlength']){
+        if(name=='usuario'){
+          this.mensajeError.usuario="Mínimo de caracteres 5"; 
+        }else{
+          this.mensajeError.password="Mínimo de caracteres 4";
+        }
+        
+      }
+    }
+    return this.formLogin.get(name)?.errors && (this.formLogin.get(name)?.touched || this.formLogin.get(name)?.dirty);
+  }
 }
